a559449d0788f2eed5596ad4d79be6d7888fc958
JEFFERSON DIEGO LIMA DA SILVA <jeffersonsilva0425@digitalcollege.internal>
2025-04-29 20:09:45 -0300

    Iniciando Const no Js

---
+++ aula01.js
@@ -0,0 +1,10 @@
+//criação de variável (const, let, var)
+const n1 = 10;
+const n2 = 40;
+
+//soma
+const resultado = n1 + n2
+
+
+//exibir o resultado da soma
+console.log(resultado)
---
+++ tipos_de _dados.js
@@ -0,0 +1,27 @@
+//var, const e let
+
+//1-const (constante)
+const pi = 3.14;
+//imprimindo a variável no console
+//console.log(pi);
+
+//2 - let (o valor da variável pode ser alterado)
+let nome = 'Angelo'
+
+let nome_atualizado = 'Angelo Lindão'
+console.log(nome);
+
+let desconto = 20.5
+//multiplicando a variável, sem alterar o valor original
+console.log(desconto *2)
+
+//Atualizando o valor da variavel utilizando a própria variável
+desconto = desconto * 2
+console.log('Desconto atualizado: ' + desconto)
+
+//Array (lista)
+let tamanhos = ['PP', 'P', 'M', 'G', 36, 38, 40, 54];
+
+let temPet = false;
+
+const idade = 28;
